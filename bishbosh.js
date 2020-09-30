bc51d1cf9427580e79778c540ef1142a3533fb3d
joakimmalmstrom <joakim.s.malmstrom@gmail.com>
2020-09-30 16:54:21 +0200

    Updated version

--- bishbosh.js
+++ bishbosh.js
@@ -2,32 +2,31 @@ let bishString = "Bish";
 let boshString = "Bosh";
 let bishBoshString = "Bish-Bosh";
 
-function callBishBosh() {
-    let bishNumber = document.getElementById("bish").value;
-    let boshNumber = document.getElementById("bosh").value;
-    let loopNumber = document.getElementById("loop").value;
-    
+// On button click with id of "submit" || Call callBishBosh()
+document.getElementById("submit").addEventListener("click", callBishBosh);
+
+function callBishBosh(loopNumber, bishNumber, boshNumber) {
+    loopNumber = document.getElementById("loop").value;
+    bishNumber = document.getElementById("bish").value;
+    boshNumber = document.getElementById("bosh").value;
+
     for (let i = 1; i <= loopNumber; i++) {
         let bish = i % bishNumber;
         let bosh = i % boshNumber;
 
-        if (bish === 0 && bosh === 0)
-        {
+        if (bish === 0 && bosh === 0) {
             document.write(bishBoshString);
             document.write("<br>");
         }
-        else if (bish === 0)
-        {
+        else if (bish === 0) {
             document.write(bishString);
             document.write("<br>");
         }
-        else if (bosh === 0)
-        {
+        else if (bosh === 0) {
             document.write(boshString);
             document.write("<br>");
         }
-        else
-        {
+        else {
             document.write(i);
             document.write("<br>");
         }
